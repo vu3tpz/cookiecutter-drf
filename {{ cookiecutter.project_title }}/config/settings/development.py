569ceea1369137c301320436cce5bd9699b47cc3
Vishnu M <vishnuvmk029@gmail.com>
2024-03-30 17:12:54 +0530

    change db setup

--- {{ cookiecutter.project_title }}/config/settings/development.py
+++ {{ cookiecutter.project_title }}/config/settings/development.py
@@ -4,22 +4,24 @@ DEBUG = True
 
 # Database Connection
 # ------------------------------------------------------------------------------
-if "{{ cookiecutter.local_database }}" == "PostgreSQL":
-    DATABASES = {
-        # default database user and credentials | others are added on runtime
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": env.str("DATABASE_DB", default=""),
-            "USER": env.str("DATABASE_USER", default=""),
-            "PASSWORD": env.str("DATABASE_PASSWORD", default=""),
-            "HOST": env.str("DATABASE_HOST", default=""),
-            "PORT": env.str("DATABASE_PORT", default=""),
-        }
+{{cookiecutter.local_database}}
+"{% if cookiecutter.local_database == 'PostgreSQL' %}"
+DATABASES = {
+    # default database user and credentials | others are added on runtime
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("DATABASE_DB", default=""),
+        "USER": env.str("DATABASE_USER", default=""),
+        "PASSWORD": env.str("DATABASE_PASSWORD", default=""),
+        "HOST": env.str("DATABASE_HOST", default=""),
+        "PORT": env.str("DATABASE_PORT", default=""),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": ROOT_DIR / "db.sqlite3",
-        }
+}
+"{% else %}"
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ROOT_DIR / "db.sqlite3",
     }
+}
+"{% endif %}"
